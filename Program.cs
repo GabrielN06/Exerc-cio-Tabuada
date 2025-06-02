55c0c91bc95964fe495e7d676680d9a27b004d7a
Erik <eriksabino@gmail.com>
2025-06-02 08:14:01 -0300

    Programa Finalizado

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.Write("Digite um número para saber sua tabuada: ");
+
+int numero = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine($"{numero} x {i} = {numero * i}");
+}
